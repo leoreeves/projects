bff38728e833675b9b77d1021138275824d654c8
Leo Reeves <leojreeves@gmail.com>
2016-11-24 17:25:12 +0000

    Update app.js

--- UK-VAT-Calculator/app.js
+++ UK-VAT-Calculator/app.js
@@ -1,13 +1,16 @@
 function calculate() {
 	var x = document.getElementById("user-input").value;
 	var vatRate = document.getElementById("VAT-rate").value;
-	if (document.getElementById("add").checked) {
+	if(document.getElementById("user-input").value === '') {
+		alert('Please enter an amount');
+	}
+	if(document.getElementById("add").checked && (document.getElementById("user-input").value !== '')) {
 		document.getElementById('exclude-VAT').style.display = 'none';
 		document.getElementById('add-VAT').style.display = 'block';
 		document.getElementById("plus-net").innerHTML = '£' + parseFloat(x).toFixed(2);
 		document.getElementById("plus-VAT").innerHTML = '£' + (x * vatRate - x).toFixed(2);
-		document.getElementById("plus-gross").innerHTML = '£' + (x * vatRate).toFixed(2);
-	} else if (document.getElementById("exclude").checked) {
+		document.getElementById("plus-gross").innerHTML = '£' + (x * vatRate).toFixed(2);	
+	} else if(document.getElementById("exclude").checked && (document.getElementById("user-input").value !== '')) {
 		document.getElementById('add-VAT').style.display = 'none';
 		document.getElementById('exclude-VAT').style.display = 'block';
 		document.getElementById("minus-net").innerHTML = '£' + parseFloat(x).toFixed(2);
