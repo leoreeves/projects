c967a861a64242a4dbed5e6a04e3ec09b486a7e5
Leo Reeves <leojreeves@gmail.com>
2016-12-01 16:19:54 +0000

    Update app.js

--- character-finder/app.js
+++ character-finder/app.js
@@ -1,30 +1,27 @@
-function charFind(){
+function charFind() {
 	var x = document.getElementById('change').value;
 	var y = document.getElementById('num').value - 1;
 	document.getElementById('num').setAttribute("max", x.length);
 	document.getElementById('count').innerHTML = '<b>' + x.length + ' characters' + '</b>';
-	if(y+1 > x.length || y <= -1) {
+	if (y + 1 > x.length || y <= -1) {
 		document.getElementById('char').innerHTML = '&nbsp;';
-		} else {
+	} else {
 		document.getElementById('char').innerHTML = x[y];
 	}
 }
-
 // clipboard button
 var clipboard = new Clipboard('.copyBtn');
-
 // clipboard success
-function show(){
+function show() {
 	document.getElementById('success').style.visibility = "";
 	document.getElementById('success').classList.add("fadeOut");
-	setTimeout(function(){
+	setTimeout(function() {
 		document.getElementById('success').style.visibility = "hidden";
 		document.getElementById('success').classList.remove("fadeOut");
-	},2000);
+	}, 2000);
 }
-
 // switch to password
-function secure(){
+function secure() {
 	if (document.getElementById('change').type == 'password') {
 		document.getElementById('change').type = 'text';
 	} else if (document.getElementById('change').type == 'text') {
