90c1019488af9c5696ea9dbd665454bc77485813
Leo Reeves <leojreeves@gmail.com>
2016-12-01 16:19:31 +0000

    Update app.js

--- character-finder/app.js
+++ character-finder/app.js
@@ -3,7 +3,9 @@ function charFind(){
 	var y = document.getElementById('num').value - 1;
 	document.getElementById('num').setAttribute("max", x.length);
 	document.getElementById('count').innerHTML = '<b>' + x.length + ' characters' + '</b>';
-	if(document.getElementById('num').value) {
+	if(y+1 > x.length || y <= -1) {
+		document.getElementById('char').innerHTML = '&nbsp;';
+		} else {
 		document.getElementById('char').innerHTML = x[y];
 	}
 }
@@ -11,9 +13,21 @@ function charFind(){
 // clipboard button
 var clipboard = new Clipboard('.copyBtn');
 
-document.getElementsByClassName('.copyBtn').addEventListener("click", show);
-
-function show() {
-	document.getElementById('success').style.display = "block";
+// clipboard success
+function show(){
+	document.getElementById('success').style.visibility = "";
+	document.getElementById('success').classList.add("fadeOut");
+	setTimeout(function(){
+		document.getElementById('success').style.visibility = "hidden";
+		document.getElementById('success').classList.remove("fadeOut");
+	},2000);
 }
 
+// switch to password
+function secure(){
+	if (document.getElementById('change').type == 'password') {
+		document.getElementById('change').type = 'text';
+	} else if (document.getElementById('change').type == 'text') {
+		document.getElementById('change').type = 'password';
+	}
+}
