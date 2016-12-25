e7f1834457edbe7fa17722346b2e9a51b2f591fc
Leo Reeves <leojreeves@gmail.com>
2016-12-25 20:08:17 +0000

    Added Code Editor

---
+++ Live Code Editor/app.js
@@ -0,0 +1,14 @@
+function compile() {
+    const html = document.getElementById("html");
+    const css = document.getElementById("css");
+    const js = document.getElementById("js");
+    const code = document.getElementById("code").contentWindow.document;
+
+    document.body.onkeyup = function() {
+        code.open();
+        code.writeln(html.value+"<style>"+css.value+"</style>"+"<script>"+js.value+"</script>");
+        code.close();
+    };
+}
+
+compile();
