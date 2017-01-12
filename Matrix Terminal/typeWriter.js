15d5d60197d134b43e219ab3313c691f5286a407
Leo Reeves <leojreeves@gmail.com>
2017-01-12 19:41:36 +0000

    Renamed file

--- Matrix Terminal/typeWriter.js
+++ Matrix Terminal/typeWriter.js
@@ -78,22 +78,3 @@ var typeWriter = function (selector, type, interval) {
     }
 
 }
-
-typeWriter("#code","true", 80);
-
-const code = document.getElementById('code');
-const text = ['Wake up, Neo...', 'The Matrix has you...', 'Follow the white rabbit.', 'Knock, knock, Neo.', ''];
-let counter = 0;
-
-setInterval(changeText, 5000);
-
-function changeText() {
-	if (counter >= text.length) {
-		counter = 4;
-		code.innerHTML = '';
-	}
-	code.innerHTML = text[counter];
-	typeWriter("#code","true", 80);
-	code.innerHTML = '';
-	counter++;
-}
