b8080fa8824b4556ac6241f205587dad098cc272
Leo Reeves <leojreeves@gmail.com>
2017-03-11 18:29:09 +0000

    Added red border validation,  remove excess

--- rental-rate-converter/app.js
+++ rental-rate-converter/app.js
@@ -1,27 +1,31 @@
 const eventType = ['change', 'keyup'];
 const perWeek = document.querySelector('.per-week');
 const perMonth = document.querySelector('.per-month');
-const inputPlaceholder = 'Please enter a number';
+const redBorder = '3px solid rgba(198, 40, 40, 0.6)';
+const defaultBorder = '3px solid #9E9E9E';
 
 eventType.forEach(e => perWeek.addEventListener(e, () => {
   // Check if there is an amount in both inputs
   if (perWeek.value === '') {
-    perMonth.value = '';
-    perMonth.placeholder = inputPlaceholder;
+    // Validation red border
+    perWeek.style.border = redBorder;
     perWeek.focus();
   } else {
     // Calculate amount per month
     perMonth.value = (perWeek.value / 7 * 365.25 / 12).toFixed(2);
+    perWeek.style.border = defaultBorder;
+    perMonth.style.border = defaultBorder;
   }
 }));
 
 eventType.forEach(e => perMonth.addEventListener(e, () => {
   if (perMonth.value === '') {
-    perWeek.value = '';
-    perWeek.placeholder = inputPlaceholder;
+    perMonth.style.border = redBorder;
     perMonth.focus();
   } else {
     // Calculate amount per week
     perWeek.value = (perMonth.value * 12 / 365.25 * 7).toFixed(2);
+    perWeek.style.border = defaultBorder;
+    perMonth.style.border = defaultBorder;
   }
 }));
