6fe76ac449f1313f8e0a6e7413a3c0ad632b9620
Leo Reeves <leojreeves@gmail.com>
2017-03-14 12:15:24 +0000

    Replaced DOM border method

--- rental-rate-converter/app.js
+++ rental-rate-converter/app.js
@@ -1,31 +1,31 @@
 const eventType = ['change', 'keyup'];
 const perWeek = document.querySelector('.per-week');
 const perMonth = document.querySelector('.per-month');
-const redBorder = '3px solid rgba(198, 40, 40, 0.6)';
-const defaultBorder = '3px solid #9E9E9E';
+const redBorder = 'rgba(198, 40, 40, 0.6)';
+const defaultBorder = '#9E9E9E';
 
 eventType.forEach(e => perWeek.addEventListener(e, () => {
   // Check if there is an amount in both inputs
   if (perWeek.value === '') {
     // Validation red border
-    perWeek.style.border = redBorder;
+    perWeek.style.borderColor = redBorder;
     perWeek.focus();
   } else {
     // Calculate amount per month
     perMonth.value = (perWeek.value / 7 * 365.25 / 12).toFixed(2);
-    perWeek.style.border = defaultBorder;
-    perMonth.style.border = defaultBorder;
+    perWeek.style.borderColor = defaultBorder;
+    perMonth.style.borderColor = defaultBorder;
   }
 }));
 
 eventType.forEach(e => perMonth.addEventListener(e, () => {
   if (perMonth.value === '') {
-    perMonth.style.border = redBorder;
+    perMonth.style.borderColor = redBorder;
     perMonth.focus();
   } else {
     // Calculate amount per week
     perWeek.value = (perMonth.value * 12 / 365.25 * 7).toFixed(2);
-    perWeek.style.border = defaultBorder;
-    perMonth.style.border = defaultBorder;
+    perWeek.style.borderColor = defaultBorder;
+    perMonth.style.borderColor = defaultBorder;
   }
 }));
