3e2d4c73c1265e7ba5e57947befe782ca90ea89d
Leo Reeves <leojreeves@gmail.com>
2017-03-20 16:27:51 +0000

    Update app.js

--- pomodoro-timer/app.js
+++ pomodoro-timer/app.js
@@ -1,5 +1,6 @@
 const timer = document.querySelector('.timer');
 const startButton = document.querySelector('.start-button');
+const stopButton = document.querySelector('.stop-button');
 const resetButton = document.querySelector('.reset-button');
 const customMinutes = document.querySelector('.custom-minutes');
 const changePomodoro = document.querySelector('.change-pomodoro');
@@ -7,15 +8,42 @@ const timerVar = setInterval(countTimer, 1);
 let totalSeconds = 1500;
 let counting = false;
 
+// Desktop Notifications
+// http://stackoverflow.com/questions/2271156/chrome-desktop-notification-example
+document.addEventListener('DOMContentLoaded', function () {
+  if (!Notification) {
+    alert('Desktop notifications not available in your browser. Try Chromium.'); 
+    return;
+  }
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+});
+
+function notifyMe() {
+  if (Notification.permission !== "granted")
+    Notification.requestPermission();
+  else {
+    const notification = new Notification('Notification title', {
+      icon: 'http://www.iconsdb.com/icons/preview/soylent-red/tomato-xxl.png',
+      body: "Timer finished!",
+    });
+  }
+}
+
 startButton.addEventListener('click', () => {
   if (counting === false && totalSeconds > 0) {
     counting = true;
-    startButton.innerHTML = 'Pause';
-  } else {
-    counting = false;
-    startButton.innerHTML = 'Start';
   }
 });
+
+stopButton.addEventListener('click', () => {
+  if (counting === true) {
+    counting = false;
+  } else {
+    counting = true;
+  }
+});
+
 
 resetButton.addEventListener('click', () => {
   counting = false;
@@ -59,9 +87,9 @@ function countTimer() {
       timer.innerHTML = `${hour < 10 ? '0' : ''}${hour}:${minute < 10 ? '0' : ''}${minute}:${seconds < 10 ? '0' : ''}${seconds}`;
     }
     if (totalSeconds === 0) {
-      // alert('Timer complete');
       counting = false;
       startButton.innerHTML = 'Start';
+      notifyMe();
     }
   }
 }
