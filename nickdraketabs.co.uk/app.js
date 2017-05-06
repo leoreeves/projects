68bead59221fa8a58857c354d6b6d23b55f82a08
Leo Reeves <leo@rapidtender.com>
2017-05-06 19:54:22 +0100

    Redesigned tab section and added navbar

---
+++ nickdraketabs.co.uk/app.js
@@ -0,0 +1,7 @@
+// window.addEventListener('scroll', function () {
+//   if (window.scrollTop() > 1) {
+//     document.querySelector('nav').style.position = 'fixed';
+//   } else {
+//     document.querySelector('nav').style.position = 'static';
+//   }
+// });
