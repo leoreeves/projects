63de7bd59ca76fda31c850c9d6d24400c2f909ad
Leo Reeves <leo@rapidtender.com>
2017-05-07 11:15:28 +0100

    Restyled using Bootstrap 4

--- nickdraketabs.co.uk/app.js
+++
@@ -1,7 +0,0 @@
-// window.addEventListener('scroll', function () {
-//   if (window.scrollTop() > 1) {
-//     document.querySelector('nav').style.position = 'fixed';
-//   } else {
-//     document.querySelector('nav').style.position = 'static';
-//   }
-// });
