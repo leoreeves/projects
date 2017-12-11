7b26d8fc1adea08fb88e7d8d86434aac49305f58
Leo Reeves <leojreeves@gmail.com>
2017-12-11 22:52:51 +0000

    Updated vat calculator and fixes to responsiveness

--- uk-vat-calculator/app.js
+++ uk-vat-calculator/app.js
@@ -3,31 +3,29 @@ const calculateVat = new Vue({
   el: '#calculate-vat',
   data: {
     inputAmount: '',
-    addOrMinusVat: '',
+    addOrMinusVat: 'add',
     vatRate: 1.2,
     netAmount: 0,
     vatAmount: 0,
     grossAmount: 0,
-    bodyStyle: 'block',
     resultVisibility: 'none',
     copyMessage: 'Click an amount to copy',
   },
   methods: {
     calculateVat() {
-      if (window.matchMedia('(max-width: 414px').matches) {
-        this.bodyStyle = 'inline';
-      }
-      // Show result card
+      // result card
       this.resultVisibility = 'block';
       const inputAmount = this.inputAmount;
       const vatRate = this.vatRate;
       this.netAmount = `£${(inputAmount).toFixed(2)}`;
       this.vatAmount = `£${((inputAmount * vatRate) - inputAmount).toFixed(2)}`;
+
       if (this.addOrMinusVat === 'add') {
         this.grossAmount = `£${(inputAmount * vatRate).toFixed(2)}`;
       } else if (this.addOrMinusVat === 'minus') {
         this.grossAmount = `£${(inputAmount / vatRate).toFixed(2)}`;
       }
+
       // Jump to bottom on smaller widths to show result
       // Added delay to allow time to render
       setTimeout(() => {
@@ -38,7 +36,7 @@ const calculateVat = new Vue({
       window.scrollTo(0, document.body.scrollHeight);
     },
     copyText() {
-      this.copyMessage = 'Sucessfully copied';
+      this.copyMessage = 'Successfully copied';
       setTimeout(() => {
         this.copyMessage = 'Click an amount to copy';
       }, 1000);
