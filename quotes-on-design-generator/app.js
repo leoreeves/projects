8163e6dde1fc8aa55f13fefed94f8dfdd03fbf41
Leo Reeves <leojreeves@gmail.com>
2018-02-24 15:39:08 +0000

    [refactor] app.js

--- quotes-on-design-generator/app.js
+++ quotes-on-design-generator/app.js
@@ -1,43 +1,43 @@
 // Generate quote when spacebar is pressed
 $(window).keypress((e) => {
   if (e.which === 32) {
-    $('.quote-body').fadeOut();
-    $('.quote-author').fadeOut();
-    setTimeout(() => {
-      $.ajax({
-        crossOrigin: true,
-        url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback',
-        dataType: 'jsonp',
-      });
-    }, 200);
+    fadeOutQuote();
+    getQuote();
   }
 });
 
 // Generate quote on click
 $(document).on('click', () => {
+  fadeOutQuote();
+  getQuote();
+});
+
+function fadeOutQuote() {
   $('.quote-body').fadeOut();
   $('.quote-author').fadeOut();
+}
+
+function getQuote() {
   setTimeout(() => {
     $.ajax({
       crossOrigin: true,
-      url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=mycallback',
+      url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=createQuote',
       dataType: 'jsonp',
     });
   }, 200);
-});
+}
 
-// Add json to elements
-function mycallback(json) {
+function createQuote(json) {
   const quote = json[0];
   $('.quote-body').html(`${quote.content}`).fadeIn();
   $('.quote-author').html(`― ${quote.title}`).fadeIn();
 }
 
-// Change click to tap on mobiles/tablets
+// Change wording on mobiles/tablets - 'click' to 'tap'
 // Thanks to Santhosh @ http://stackoverflow.com/a/29509267/2588066
 $(document).ready(() => {
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
   if (isMobile) {
-    $('.quote-body').html('Press the spacebar or tap to inspire');
+    $('#input-type').html('tap');
   }
 });
