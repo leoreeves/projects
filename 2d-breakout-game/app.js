2d6d0486d6bd4afa4c189fc904d97b7d7653ec99
Leo Reeves <leojreeves@gmail.com>
2018-02-25 22:06:26 +0000

    [styling] Switched font to open sans

--- 2d-breakout-game/app.js
+++ 2d-breakout-game/app.js
@@ -95,13 +95,13 @@ function collisionDetection() {
 }
 
 function drawScore() {
-  ctx.font = '16px Arial';
+  ctx.font = '16px Open Sans';
   ctx.fillStyle = '#2196F3';
   ctx.fillText(`Score: ${score}`, 8, 20);
 }
 
 function drawLives() {
-  ctx.font = '16px Arial';
+  ctx.font = '16px Open Sans';
   ctx.fillStyle = '#2196F3';
   ctx.fillText(`Lives: ${lives}`, canvas.width - 65, 20);
 }
