ac9e09752830c437b494100a043c8460a4c9b196
Leo Reeves <leojreeves@gmail.com>
2018-02-25 14:49:41 +0000

    [feature] Cleaned up unused code

--- birthday-calculator/app.js
+++ birthday-calculator/app.js
@@ -6,22 +6,17 @@ const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Frida
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
   'September', 'October', 'November', 'December'];
 
-const minus = document.querySelector('.minus');
-const plus = document.querySelector('.plus');
-
-minus.addEventListener('click', () => {
-  birthdayYear.value -= 1;
-});
-
-plus.addEventListener('click', () => {
-  birthdayYear.value = Number(birthdayYear.value) + 1;
-});
-
 function getBirthdayDay() {
   const birthDate = new Date(dateOfBirth.value);
   const myDate = new Date(`${months[birthDate.getMonth()]} ${birthDate.getDate()}, ${birthdayYear.value}`);
+
   if (dateOfBirth.value !== '' && birthDate.getFullYear() >= 1900) {
     birthdayWeekday.innerHTML = weekdays[myDate.getDay()];
     age.innerHTML = birthdayYear.value - birthDate.getFullYear();
   }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+  // set current year
+  birthdayYear.value = new Date().getFullYear();
+});
