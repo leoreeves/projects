054d317d6bca620a70504c920b22b2279556348e
Leo Reeves <leojreeves@gmail.com>
2018-03-03 19:17:52 +0000

    [config] Server.js

--- ionic-contacts-manager/server.js
+++ ionic-contacts-manager/server.js
@@ -1,23 +1,20 @@
-var express  = require('express');
-var app      = express();
-var morgan = require('morgan');
-var bodyParser = require('body-parser');
-var cors = require('cors');
-
-app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({'extended':'true'}));
-app.use(bodyParser.json());
-app.use(cors());
-
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'DELETE, PUT');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+var express = require('express'),
+    app = express();
 
 app.use(express.static('www'));
+
+// CORS (Cross-Origin Resource Sharing) headers to support Cross-site HTTP requests
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
+// API Routes
+// app.get('/blah', routeHandler);
+
 app.set('port', process.env.PORT || 5000);
+
 app.listen(app.get('port'), function () {
-  console.log('Express server listening on port ' + app.get('port'));
+    console.log('Express server listening on port ' + app.get('port'));
 });
