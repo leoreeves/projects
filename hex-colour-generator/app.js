837d613b4c415f09abc8c4967b485248a860c299
Leo Reeves <leojreeves@gmail.com>
2018-11-13 21:13:32 +0000

    [refactor/fix] Hex colour generator

--- hex-colour-generator/app.js
+++ hex-colour-generator/app.js
@@ -1,60 +1,77 @@
-function getColor() {
+const hexColorHeader = document.querySelector('.container__hex-colour-header');
+const copyToClipboardButton = document.querySelector('.container__copy-to-clipboard-button');
+const clipboard = new ClipboardJS(copyToClipboardButton);
+
+function generateColor() {
   return `#${Math.random().toString(16).slice(2, 8)}`;
 }
 
-function setBackground() {
-  const backgroundColour = getColor();
+// based on: http://stackoverflow.com/a/11868398/2588066
+function getContrastYIQ(colorCode) {
+  const [r, g, b] = [0, 2, 4].map(p => parseInt(colorCode.substr(p, 2), 16));
+  return ((r * 299) + (g * 587) + (b * 114)) / 1000;
+}
 
-  // need to remove the '#' from hex code
-  const hexCode = backgroundColour.slice(1, 7);
-  document.body.style.backgroundColor = backgroundColour;
-  document.querySelector('.container--hex-colour-code').textContent = backgroundColour;
+function setBackgroundColor(color) {
+  document.body.style.backgroundColor = color;
+}
 
-  /**
-   * change text colour based on background brightness -
-   * based on this: http://stackoverflow.com/a/11868398/2588066
-   **/
-  const getContrastYIQ = hexCode => {
-    const [r, g, b] = [0, 2, 4].map(p => parseInt(hexCode.substr(p, 2), 16));
-    return ((r * 299) + (g * 587) + (b * 114)) / 1000 >= 128;
-  }
-  const colorContainer = document.querySelector('.container--colour-generator');
+function setHexColorHeaderText(colorCode) {
+  hexColorHeader.textContent = colorCode;
+}
 
-  if (getContrastYIQ >= 128) {
-    colorContainer.style.color = '#000';
+function setDataClipboardTextAttribute(colorCode) {
+  copyToClipboardButton.setAttribute('data-clipboard-text', colorCode);
+}
+
+function setTextColorBasedOnContrastYIQ(color) {
+  const colorCodeWithoutHash = color.slice(1, 7);
+  const contrastYIQ = getContrastYIQ(colorCodeWithoutHash);
+
+  if (contrastYIQ >= 128) {
+    document.body.style.color = '#000';
   } else {
-    colorContainer.style.color = '#fff';
+    document.body.style.color = '#fff';
   }
+}
+
+function generateColorAndUpdatePage() {
+  const color = generateColor();
+  setBackgroundColor(color);
+  setDataClipboardTextAttribute(color);
+  setHexColorHeaderText(color);
+  setTextColorBasedOnContrastYIQ(color);
+}
+
+function showAndFadeOutSuccessMessage() {
+  const successMessageContainer = document.querySelector('.container__success-message');
+  const successMessageFadeOutClass = 'container__success-message--fade-out';
+  successMessageContainer.style.display = 'block';
+  successMessageContainer.classList.add(successMessageFadeOutClass);
+  copyToClipboardButton.blur();
+  document.getSelection().removeAllRanges();
+  setTimeout(() => {
+    successMessageContainer.classList.remove(successMessageFadeOutClass);
+  }, 1800);
 }
 
 // change colour on spacebar press
 document.body.onkeyup = (e) => {
   if (e.keyCode === 32) {
-    setBackground();
+    generateColorAndUpdatePage();
   }
 };
 
 // change colour on mouse click
 document.addEventListener('click', (e) => {
-  const successMessageContainer = document.querySelector('.container--success-message');
-
-  if (e.target.className === 'button--copy-to-clipboard') {
-    successMessageContainer.style.display = 'block';
-    successMessageContainer.classList.add('fade-out');
-    document.querySelector('button').blur();
-    document.getSelection().removeAllRanges();
-    setTimeout(function () {
-      successMessageContainer.classList.remove('fade-out');
-    }, 1800);
+  if (e.target.className === 'container__copy-to-clipboard-button') {
+    showAndFadeOutSuccessMessage();
   } else {
-    setBackground();
+    generateColorAndUpdatePage();
   }
-
 });
-// initialise copy to clipboard
-const clipboard = new Clipboard('.button--copy-to-clipboard');
 
 // replace text on mobile
 if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini|Mobile|mobile/i.test(navigator.userAgent)) {
-  document.querySelector('.header--instruction').textContent = 'Tap to generate a new colour';
+  document.querySelector('.container__instruction-header').textContent = 'Tap to generate a new colour';
 }
