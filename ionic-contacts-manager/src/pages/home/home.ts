290f275c6cdb7b6af3219f382994422798734116
Leo Reeves <leojreeves@gmail.com>
2019-02-21 20:12:18 +0000

    [refactor] Contact management modals and methods

--- ionic-contacts-manager/src/pages/home/home.ts
+++ ionic-contacts-manager/src/pages/home/home.ts
@@ -1,45 +1,29 @@
-import { Component } from '@angular/core';
-import { ModalController, NavController } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { ModalController, ToastController } from 'ionic-angular';
 import { Storage } from '@ionic/storage';
+
+import { Contact } from '../../models/contact';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
-  contacts: Array<any> = [];
+export class HomePage implements OnInit {
+  contacts: Contact[] = [];
 
   constructor(
-    public navCtrl: NavController,
-    public modalCtrl: ModalController,
-    public storage: Storage
-  ) {
-  }
+    private modalCtrl: ModalController,
+    private storage: Storage,
+    private toastCtrl: ToastController,
+  ) {}
 
-  addMockContacts() {
-    this.contacts = [
-      // 'Ludie Straley',
-      // 'Vannessa Tilson',
-      // 'Masako Manville',
-      // 'Elva Neuner',
-      // 'Lacy Paschal',
-      // 'Branda Lipsky',
-    ]
-  }
-
-  setContactsData() {
-    console.log('set');
-    this.storage.set('key', JSON.stringify(this.contacts))
-      .then(() =>
-        this.storage.get('key')
-          .then((value) => {
-            this.contacts = JSON.parse(value);
-          })
-      )
+  ngOnInit() {
+    this.getContactsData();
+    this.addMockContacts();
+    this.sortContactsByName();
   }
 
   async getContactsData() {
-    console.log('get')
     return await this.storage.get('key')
       .then((val) => {
         if (val) {
@@ -49,8 +33,47 @@ export class HomePage {
       })
   }
 
-  sortContacts() {
+  sortContactsByName() {
     this.contacts = this.contacts.sort((a, b) => a.name.localeCompare(b.name));
+  }
+
+  addMockContacts() {
+    this.contacts = [
+      {
+        name: 'Ludie Straley',
+        email: '',
+        phone: '07700 900526',
+        address: ''
+      },
+      {
+        name: 'Vannessa Tilson',
+        email: '',
+        phone: '07430 230572',
+        address: ''
+      },
+      {
+        name: 'Masako Manville',
+        email: '',
+        phone: '07302 900512',
+        address: ''
+      },
+      {
+        name: 'Ludie Straley',
+        email: '',
+        phone: '07340 933542',
+        address: ''
+      }
+    ]
+  }
+
+  setContactsStorageData() {
+    this.storage.set('key', JSON.stringify(this.contacts))
+      .then(() =>
+        this.storage.get('key')
+          .then((value) => {
+            this.contacts = JSON.parse(value);
+          })
+      )
   }
 
   openAddContactModal() {
@@ -63,56 +86,56 @@ export class HomePage {
     });
   }
 
-  openEditContactModal(contact) {
-    const editContactModal = this.modalCtrl.create('manage-contact-modal', { modalType: 'Edit', contactData: contact });
-    editContactModal.present();
-    editContactModal.onDidDismiss((originalContact, updatedContact) => {
-      if (originalContact && updatedContact) {
-        this.updateContact(originalContact, updatedContact);
+  openManageContactModal(contact) {
+    const openManageContactModal = this.modalCtrl.create('manage-contact-modal', { modalType: 'Edit', contactData: contact });
+    openManageContactModal.present();
+    openManageContactModal.onDidDismiss((data) => {
+      if (data) {
+        this.updateContact(data.originalContact, data.updatedContact);
       }
     });
   }
 
-  openViewContactModal(contact) {
+  openViewContactModal(contact: Contact) {
     const viewContactModal = this.modalCtrl.create('view-contact-modal', { contactData: contact });
     viewContactModal.present();
-    viewContactModal.onDidDismiss((method, contact) => {
-      if (method === 'delete') {
-        this.deleteContact(contact);
-      } else if (method === 'edit') {
-        this.openEditContactModal(contact);
+    viewContactModal.onDidDismiss((data) => {
+      if (data.method === 'delete') {
+        this.deleteContact(data.contact);
+      } else if (data.method === 'edit') {
+        this.openManageContactModal(data.contact);
       }
     })
   }
 
-  addContact(details) {
-    if (details) {
-      this.contacts.push(details);
-      this.sortContacts();
-      this.setContactsData();
+  addContact(contact: Contact) {
+    if (contact) {
+      this.contacts.push(contact);
+      this.sortContactsByName();
+      this.setContactsStorageData();
+      this.presentToast('Loading').then(() => this.presentToast('New contact created'));
     }
   }
 
-  updateContact(originalContact, updatedContact) {
+  updateContact(originalContact: Contact, updatedContact: Contact) {
     if (updatedContact) {
       this.contacts = this.contacts.filter(contact => contact !== originalContact);
       this.addContact(updatedContact);
     }
   }
 
-  deleteContact(contact) {
+  deleteContact(contact: Contact) {
     if (contact) {
       this.contacts = this.contacts.filter(contacts => contacts != contact);
-      this.setContactsData();
-      console.log(`${contact.name} deleted`);
+      this.setContactsStorageData();
+      this.presentToast(`${contact.name} was deleted`)
     }
   }
 
-  filterContacts(ev) {
+  filterContacts(event) {
     this.getContactsData()
       .then(() => {
-        console.log(this.contacts);
-        const val = ev.target.value;
+        const val = event.target.value;
 
         if (val && val.trim() !== '') {
           this.contacts = this.contacts.filter(contact => contact.name.toLowerCase().includes(val.toLowerCase()));
@@ -120,8 +143,11 @@ export class HomePage {
       })
   }
 
-  ionViewDidLoad() {
-    this.getContactsData();
-    this.sortContacts();
+  presentToast(message: string): Promise<any> {
+    const toast = this.toastCtrl.create({
+      message: message,
+      duration: 2500,
+    });
+    return toast.present();
   }
 }
