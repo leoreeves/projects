8e2d733582c77ae662e71e92bfba6817bd70212c
Leo Reeves <leojreeves@gmail.com>
2019-04-19 10:35:04 +0100

    [feat] Add fetchLocations method

--- react-map-pins/src/App.js
+++ react-map-pins/src/App.js
@@ -3,10 +3,28 @@ import './App.css';
 import Map from './components/map/Map';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      locations: []
+    }
+  }
+
+  fetchLocations() {
+    fetch('https://s3-eu-west-1.amazonaws.com/omnifi/techtests/locations.json')
+      .then(response => response.json())
+      .then(data => this.setState({locations: data}))
+      .catch(error => console.error(error));
+  }
+
+  componentDidMount() {
+    this.fetchLocations();
+  }
+
   render() {
     return (
       <div className="App">
-        <Map />
+        <Map locations={this.state.locations} />
       </div>
     );
   }
