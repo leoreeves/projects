6871542e5b3331905973e87b82f353a2fd2d81a0
Leo Reeves <leojreeves@gmail.com>
2019-05-10 21:12:30 +0100

    [feat] Implement inputs and calculations

--- vue-pricing-calculator/src/components/PricingCalculator.vue
+++ vue-pricing-calculator/src/components/PricingCalculator.vue
@@ -1,4 +1,15 @@
 <template>
+  <div>
+    <div>
+      <input type="number" placeholder="Reward (£)" step="0.01" v-model.number="reward">
+      <input type="number" placeholder="Number of places" v-model.number="places">
+      <input type="number" placeholder="Time to complete study (minutes)" v-model.number="time">
+    </div>
+    <div>
+      <p><span>Total cost: </span>£{{ calculateTotalCost }}</p>
+      <p><span>Reward per hour: </span>£{{ calculateRewardPerHour }}</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,6 +18,28 @@ export default {
   props: {
     feesPercentage: Number,
     vatPercentage: Number,
+  },
+  data () {
+    return {
+      reward: '',
+      places: '',
+      time: '',
+    }
+  },
+  computed: {
+    calculateTotalCost() {
+      if (this.reward && this.places) {
+        const cost = this.reward * this.places;
+        const additionalFees = cost * this.feesPercentage;
+        const vat = additionalFees * this.vatPercentage;
+        return (Math.ceil((cost + additionalFees + vat) * 100) / 100).toFixed(2);
+      }
+    },
+    calculateRewardPerHour() {
+      if (this.reward && this.time) {
+        return (Math.ceil((this.reward / this.time * 60) * 100) / 100).toFixed(2);
+      }
+    }
   }
 }
 </script>
