b9dff956a56e95afd4fb6ea079a3cc588dc4335b
Leo Reeves <leojreeves@gmail.com>
2019-05-11 11:41:19 +0100

    [refactor] Move beforEach up and additional spec

--- vue-pricing-calculator/tests/unit/pricing-calculator.spec.js
+++ vue-pricing-calculator/tests/unit/pricing-calculator.spec.js
@@ -4,32 +4,36 @@ import PricingCalculator from '@/components/PricingCalculator.vue'
 describe('PricingCalculator.vue', () => {
   let feesPercentage;
   let vatPercentage;
+  let wrapper;
+
+  beforeEach(() => {
+    feesPercentage = 0.3;
+    vatPercentage = 0.2;
+    wrapper = shallowMount(PricingCalculator, {
+      propsData: { feesPercentage, vatPercentage }
+    });
+  });
 
   it('is a Vue instance', () => {
-    const wrapper = shallowMount(PricingCalculator);
     expect(wrapper.isVueInstance()).toBeTruthy();
   })
 
   describe('computed', () => {
-    beforeEach(() => {
-      feesPercentage = 0.3;
-      vatPercentage = 0.2;
-    });
-
     it('calculates correct totalCost', () => {
-      const wrapper = shallowMount(PricingCalculator, {
-        propsData: { feesPercentage, vatPercentage }
-      });
       wrapper.setData({ reward: 1, places: 10 });
       expect(wrapper.vm.calculateTotalCost).toEqual('13.60');
     });
 
     it('calculates correct rewardPerHour', () => {
-      const wrapper = shallowMount(PricingCalculator, {
-        propsData: { feesPercentage, vatPercentage }
-      });
       wrapper.setData({ reward: 1, time: 10 });
       expect(wrapper.vm.calculateRewardPerHour).toEqual('6.00');
+    });
+  });
+
+  describe('methods', () => {
+    it('adds commas to amounts correctly', () => {
+      const addCommasTest = wrapper.vm.addCommasToAmount('2500');
+      expect(addCommasTest).toEqual('2,500');
     });
   });
 })
