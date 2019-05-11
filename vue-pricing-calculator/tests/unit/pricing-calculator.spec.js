af004ef0183eab31c0bc66bb04e7c87307adcbb3
Leo Reeves <leojreeves@gmail.com>
2019-05-11 11:26:02 +0100

    [test] Add unit tests

--- vue-pricing-calculator/tests/unit/pricing-calculator.spec.js
+++ vue-pricing-calculator/tests/unit/pricing-calculator.spec.js
@@ -1,12 +1,35 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import PricingCalculator from '@/components/PricingCalculator.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
+describe('PricingCalculator.vue', () => {
+  let feesPercentage;
+  let vatPercentage;
+
+  it('is a Vue instance', () => {
+    const wrapper = shallowMount(PricingCalculator);
+    expect(wrapper.isVueInstance()).toBeTruthy();
   })
+
+  describe('computed', () => {
+    beforeEach(() => {
+      feesPercentage = 0.3;
+      vatPercentage = 0.2;
+    });
+
+    it('calculates correct totalCost', () => {
+      const wrapper = shallowMount(PricingCalculator, {
+        propsData: { feesPercentage, vatPercentage }
+      });
+      wrapper.setData({ reward: 1, places: 10 });
+      expect(wrapper.vm.calculateTotalCost).toEqual('13.60');
+    });
+
+    it('calculates correct rewardPerHour', () => {
+      const wrapper = shallowMount(PricingCalculator, {
+        propsData: { feesPercentage, vatPercentage }
+      });
+      wrapper.setData({ reward: 1, time: 10 });
+      expect(wrapper.vm.calculateRewardPerHour).toEqual('6.00');
+    });
+  });
 })
