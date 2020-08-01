9639fe8e4d7e47d74e8d06f6e704765f73739dd9
Leo Reeves <leojreeves@gmail.com>
2020-08-01 18:40:21 +0100

    chore: update gulpfile

--- nickdraketabs.co.uk/gulpfile.js
+++ nickdraketabs.co.uk/gulpfile.js
@@ -1,15 +1,10 @@
 const gulp = require('gulp');
 const nunjucksRender = require('gulp-nunjucks-render');
 
-gulp.task('nunjucks', function() {
-  return gulp.src('src/pages/**/*.html')
+gulp.task('nunjucks', () => gulp.src('src/pages/**/*.html')
   .pipe(nunjucksRender({
-      path: ['src/templates']
-    }))
-  .pipe(gulp.dest('./'))
-});
+    path: ['src/templates'],
+  }))
+  .pipe(gulp.dest('./')));
 
-gulp.task('watch', function() {
-  /** Watch any changes */
-  return gulp.watch( 'src/pages/**/*.html', ['nunjucks'] );
-});
+gulp.task('watch', () => gulp.watch('src/pages/**/*.html', gulp.series('nunjucks')));
