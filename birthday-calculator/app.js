f9f31ffe0874fb61c7aeee1b5a84318fe4ae705b
Leo Reeves <15375448+leoreeves@users.noreply.github.com>
2022-08-16 22:05:36 +0100

    refactor: simplify calculation

--- birthday-calculator/app.js
+++ birthday-calculator/app.js
@@ -2,12 +2,9 @@ const dateOfBirthInput = document.querySelector('.date-of-birth');
 const birthdayYearInput = document.querySelector('.birthday-year');
 const birthdayWeekdayElement = document.querySelector('.birthday-weekday');
 const ageElement = document.querySelector('.age');
-const weekdays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August',
-  'September', 'October', 'November', 'December'];
-const currentYear = new Date().getFullYear();
 
 function setBirthdayYearInputValueAsCurrentYear() {
+  const currentYear = new Date().getFullYear();
   birthdayYearInput.value = currentYear;
 }
 
@@ -23,11 +20,9 @@ function updateInnerHTMLValues(calculatedWeekday, calculatedAge) {
 function calculateBirthdayWeekDayAndAge() {
   const dateOfBirth = new Date(dateOfBirthInput.value);
   const birthYear = dateOfBirth.getFullYear();
-  const birthMonth = dateOfBirth.getMonth();
-  const birthDate = dateOfBirth.getDate();
   const enteredYear = birthdayYearInput.value;
-  const calculatedDate = new Date(`${months[birthMonth]} ${birthDate}, ${enteredYear}`);
-  const calculatedWeekday = weekdays[calculatedDate.getDay()];
+  dateOfBirth.setFullYear(enteredYear);
+  const calculatedWeekday = new Intl.DateTimeFormat('en-US', { weekday: 'long' }).format(dateOfBirth);
   const calculatedAge = enteredYear - birthYear;
 
   if (dateOfBirth && enteredYear >= birthYear) {
