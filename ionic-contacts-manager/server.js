6851a1370d0b68a1de7f6f4e10757002ebde655e
Leo Reeves <15375448+leoreeves@users.noreply.github.com>
2022-08-27 13:30:35 +0100

    chore: additional linting

--- ionic-contacts-manager/server.js
+++ ionic-contacts-manager/server.js
@@ -4,5 +4,5 @@ const app = express()
 app.use(express.static('www'))
 app.set('port', process.env.PORT || 5000)
 app.listen(app.get('port'), () => {
-  console.log(`Express server listening on port ${  app.get('port')}`)
+  console.log(`Express server listening on port ${app.get('port')}`)
 })
