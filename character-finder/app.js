75d94d94c7c6f0889997258bcac51751574c0e27
Leo Reeves <15375448+leoreeves@users.noreply.github.com>
2022-08-29 12:11:33 +0100

    refactor: use init functions

--- character-finder/app.js
+++ character-finder/app.js
@@ -1,34 +1,37 @@
-const characterFinder = new Vue({
-  el: '.character-finder',
+const initialiseCharacterFinderVueInstance = () =>
+  new Vue({
+    el: '.character-finder',
 
-  data: {
-    textInput: '',
-    characterNumber: 1,
-    isActive: false,
-    show: false,
-  },
-
-  methods: {
-    incrementCharacterIndex(operator) {
-      const originalCharacterIndex = this.characterNumber
-
-      if (operator === '-' && this.characterNumber > 1) {
-        this.characterNumber = Number(originalCharacterIndex) - 1
-      } else if (operator === '+' && this.characterNumber < this.textInput.length) {
-        this.characterNumber = Number(originalCharacterIndex) + 1
-      }
+    data: {
+      textInput: '',
+      characterNumber: 1,
+      isActive: false,
+      show: false,
     },
 
-    showCopyMessage() {
-      if (this.textInput !== '') {
-        this.show = true
-        setTimeout(() => {
-          this.show = false
-        }, 1000)
-      }
-    },
-  },
-})
+    methods: {
+      incrementCharacterIndex(operator) {
+        const originalCharacterIndex = this.characterNumber
 
-// clipboard.js
-const clipboard = new ClipboardJS('.copy-button')
+        if (operator === '-' && this.characterNumber > 1) {
+          this.characterNumber = Number(originalCharacterIndex) - 1
+        } else if (operator === '+' && this.characterNumber < this.textInput.length) {
+          this.characterNumber = Number(originalCharacterIndex) + 1
+        }
+      },
+
+      showCopyMessage() {
+        if (this.textInput !== '') {
+          this.show = true
+          setTimeout(() => {
+            this.show = false
+          }, 1000)
+        }
+      },
+    },
+  })
+
+const initialiseClipboardJS = () => new ClipboardJS('.copy-button')
+
+initialiseCharacterFinderVueInstance()
+initialiseClipboardJS()
