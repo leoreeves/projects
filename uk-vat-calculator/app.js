0f12f528cf56358195f062c4fad896ed009373fc
Leo Reeves <15375448+leoreeves@users.noreply.github.com>
2022-08-30 17:31:30 +0100

    refactor: use computed values

--- uk-vat-calculator/app.js
+++ uk-vat-calculator/app.js
@@ -6,50 +6,48 @@ const initialiseVatCalculator = () =>
       inputAmount: '',
       vatOperation: 'plus',
       vatRate: 1.2,
-      netAmount: 0,
-      vatAmount: 0,
-      grossAmount: 0,
       resultCardDisplay: 'none',
       showCopiedToastMessage: false,
     },
 
-    methods: {
-      calculateAmountsAndDisplayResult() {
-        this.resultCardDisplay = 'block'
-        this.calculateNetAmount()
-        this.calculateVatAmount()
-        this.calculateGrossAmount()
-
-        setTimeout(() => {
-          this.scrollToBottom()
-        }, 1)
+    computed: {
+      netAmount() {
+        return this.formatAmount(this.inputAmount)
       },
 
+      vatAmount() {
+        const vat = this.inputAmount * this.vatRate - this.inputAmount
+        return this.formatAmount(vat)
+      },
+
+      grossAmount() {
+        let gross
+        if (this.vatOperation === 'plus') {
+          gross = this.inputAmount * this.vatRate
+        } else if (this.vatOperation === 'minus') {
+          gross = this.inputAmount / this.vatRate
+        }
+        return this.formatAmount(gross)
+      },
+    },
+
+    methods: {
       formatAmount(amount) {
         return new Intl.NumberFormat('en-GB', { style: 'currency', currency: 'GBP' }).format(amount)
-      },
-
-      calculateNetAmount() {
-        this.netAmount = this.formatAmount(this.inputAmount)
-      },
-
-      calculateVatAmount() {
-        this.vatAmount = this.formatAmount(this.inputAmount * this.vatRate - this.inputAmount)
-      },
-
-      calculateGrossAmount() {
-        if (this.vatOperation === 'plus') {
-          this.grossAmount = this.formatAmount(this.inputAmount * this.vatRate)
-        } else if (this.vatOperation === 'minus') {
-          this.grossAmount = this.formatAmount(this.inputAmount / this.vatRate)
-        }
       },
 
       scrollToBottom() {
         window.scrollTo(0, document.body.scrollHeight)
       },
 
-      handleCopiedToastMessage() {
+      handleResult() {
+        this.resultCardDisplay = 'block'
+        setTimeout(() => {
+          this.scrollToBottom()
+        }, 1)
+      },
+
+      handleCopiedToastMessageVisibility() {
         this.showCopiedToastMessage = true
         setTimeout(() => {
           this.showCopiedToastMessage = false
