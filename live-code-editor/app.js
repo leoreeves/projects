7329726e6a181d386449d0c4cddbab3fffd0c4d2
Leo Reeves <15375448+leoreeves@users.noreply.github.com>
2022-08-31 14:35:14 +0100

    refactor: improve code editor

--- live-code-editor/app.js
+++ live-code-editor/app.js
@@ -1,16 +1,22 @@
-function compile() {
+function getTextareaElements() {
   const elements = {}
   ;['html', 'css', 'js', 'code'].forEach((elementId) => {
     elements[elementId] = document.getElementById(elementId)
   })
-
-  document.body.onkeyup = () => {
-    const { html, css, js, code } = elements
-    const { document } = code.contentWindow
-    document.open()
-    document.writeln(`${html.value}<style>${css.value}</style><script>${js.value}</script>`)
-    document.close()
-  }
+  return elements
 }
 
-compile()
+function writeCodeToIframe() {
+  const textareaElements = getTextareaElements()
+  const { html, css, js, code } = textareaElements
+  const { document } = code.contentWindow
+  document.open()
+  document.writeln(`${html.value}<style>${css.value}</style><script>${js.value}</script>`)
+  document.close()
+}
+
+function addBodyKeyupEventListener() {
+  document.body.addEventListener('keyup', writeCodeToIframe)
+}
+
+addBodyKeyupEventListener()
